a2159a7a5a5648d480a19b859e0996d1d0c23840
garyellow <gary20011110@gmail.com>
2024-03-08 19:36:49 +0800

    Refactor logging format and update Chrome driver download URLs

--- auto_download.py
+++ auto_download.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from win32com import client as wincom_client
 
 load_dotenv()
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %I:%M:%S %p')
+logging.basicConfig(format="%(asctime)s %(message)s", datefmt="%Y/%m/%d %I:%M:%S %p")
 logger = logging.getLogger(__name__)
 
 if os.getenv("CHROME_PATH") is None:
@@ -17,17 +17,19 @@ if os.getenv("PLATFORM") is None:
     raise Exception("Please set PLATFORM in .env")
 
 CHROME_DRIVER_BASE_URL = "https://googlechromelabs.github.io/chrome-for-testing"
-CHROME_DRIVER_DOWNLOAD_URL = "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing"
-DOWNLOAD_FOLDER = os.getenv("DOWNLOAD_FOLDER") if os.getenv("DOWNLOAD_FOLDER") else os.getcwd()
-CHROME_DRIVER_FOLDER = "{}\\chromedriver-{}".format(DOWNLOAD_FOLDER, os.getenv("PLATFORM"))
-CHROME_DRIVER_ZIP = "{}.zip".format(CHROME_DRIVER_FOLDER)
-CHROME_DRIVER_EXE = "{}\\chromedriver.exe".format(CHROME_DRIVER_FOLDER)
+CHROME_DRIVER_DOWNLOAD_URL = "https://storage.googleapis.com/chrome-for-testing-public"
+DOWNLOAD_FOLDER = (
+    os.getenv("DOWNLOAD_FOLDER") if os.getenv("DOWNLOAD_FOLDER") else os.getcwd()
+)
+CHROME_DRIVER_FOLDER = f"{DOWNLOAD_FOLDER}\\chromedriver-{os.getenv('PLATFORM')}"
+CHROME_DRIVER_ZIP = f"{CHROME_DRIVER_FOLDER}.zip"
+CHROME_DRIVER_EXE = f"{CHROME_DRIVER_FOLDER}\\chromedriver.exe"
 
 
 def get_file_version(file_path):
     logging.info("Get file version of [%s]", file_path)
     if not os.path.isfile(file_path):
-        raise FileNotFoundError("{!r} is not found.".format(file_path))
+        raise FileNotFoundError(f"{file_path} is not found.")
 
     wincom_obj = wincom_client.Dispatch("Scripting.FileSystemObject")
     version = wincom_obj.GetFileVersion(file_path)
@@ -39,20 +41,20 @@ def get_chrome_driver_major_version():
     chrome_path = os.getenv("CHROME_PATH")
     chrome_ver = get_file_version(chrome_path)
     chrome_major_ver = chrome_ver.split(".")[0]
-    logger.info("Chrome version: {}".format(chrome_ver))
+    logger.info("Chrome version: %s", chrome_ver)
     return chrome_major_ver
 
 
 def get_latest_driver_version(browser_ver):
-    latest_api = "{}/LATEST_RELEASE_{}".format(CHROME_DRIVER_BASE_URL, browser_ver)
-    resp = requests.get(latest_api)
+    latest_api = f"{CHROME_DRIVER_BASE_URL}/LATEST_RELEASE_{browser_ver}"
+    resp = requests.get(latest_api, timeout=0)
     lastest_driver_version = resp.text.strip()
-    logger.info("Latest driver version: {}".format(lastest_driver_version))
+    logger.info("Latest driver version: %s", lastest_driver_version)
     return lastest_driver_version
 
 
 def download_driver(driver_ver, dest_folder):
-    download_api = "{}/{}/win64/chromedriver-{}.zip".format(CHROME_DRIVER_DOWNLOAD_URL, driver_ver, os.getenv("PLATFORM"))
+    download_api = f"{CHROME_DRIVER_DOWNLOAD_URL}/{driver_ver}/{os.getenv('PLATFORM')}/chromedriver-{os.getenv('PLATFORM')}.zip"
     dest_path = os.path.join(dest_folder, os.path.basename(download_api))
     resp = requests.get(download_api, stream=True, timeout=300)
 
@@ -67,9 +69,9 @@ def download_driver(driver_ver, dest_folder):
 
 
 def unzip_driver_to_target_path(src_file, dest_path):
-    with zipfile.ZipFile(src_file, 'r') as zip_ref:
+    with zipfile.ZipFile(src_file, "r") as zip_ref:
         zip_ref.extractall(dest_path)
-    logger.info("Unzip [{}] -> [{}]".format(src_file, dest_path))
+    logger.info("Unzip [%s] -> [%s]", src_file, dest_path)
 
 
 def check_browser_driver_available():
@@ -87,5 +89,7 @@ def check_browser_driver_available():
 if __name__ == "__main__":
     try:
         check_browser_driver_available()
-    except Exception as e:
+    except FileNotFoundError as e:
         logger.error(e)
+    except Exception as e:
+        logger.error("An error occurred: %s", e)
